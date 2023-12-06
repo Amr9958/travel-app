f356b03a62e22ad10b0a3339f6769aef5d7c1afb
Amr9958 <107650487+Amr9958@users.noreply.github.com>
2023-12-06 16:12:25 +0200

    comenting some thing to test commit

--- lib/views/navpages/main_page.dart
+++ lib/views/navpages/main_page.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:note_app__/main.dart';
 import 'package:note_app__/misc/colors.dart';
 import 'package:note_app__/views/navpages/BarItemPage.dart';
 import 'package:note_app__/views/navpages/HomePage.dart';
@@ -16,6 +15,7 @@ class mainpage extends StatefulWidget {
 }
 
 class _mainpageState extends State<mainpage> {
+  ////////
   int Currindx = 0;
   List pages = [HomePage(), BarItempage(), searshPage(), Mypage()];
   void Function(int)? ontap(indx) {
@@ -28,7 +28,6 @@ class _mainpageState extends State<mainpage> {
   Widget build(BuildContext context) {
     return Scaffold(
         bottomNavigationBar: BottomNavigationBar(
-   
             type: BottomNavigationBarType.shifting,
             selectedItemColor: AppColors.mainColor,
             unselectedItemColor: AppColors.mainColor.withOpacity(.3),
